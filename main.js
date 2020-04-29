b75c441649d6bd24da674da11106c916673f3912
maikel abdelmalak <maikel22@hotmail.it>
2020-04-29 16:20:31 +0200

    1

--- main.js
+++ main.js
@@ -1,9 +1,23 @@
+//visualizzare l'hamburger menu su mobile
 
+//prima versione
+// $('.header-right > a').click(function(){
+//     $('.hamburger-menu').show();
+// })
+//
+//
+// $('.close').click(function(){
+//     $('.hamburger-menu').hide();
+// })
+
+
+
+//seconda versione
 $('.header-right > a').click(function(){
-    $('.hamburger-menu').show();
+    $('.hamburger-menu').fadeIn(2000);
 })
 
 
 $('.close').click(function(){
-    $('.hamburger-menu').hide();
+    $('.hamburger-menu').fadeOut();
 })
